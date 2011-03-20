5828d83d45529f53b3dd8e2953ba301998517c80
Edoardo Batini <eodbat@gmail.com>
2011-03-20 02:14:15 +0100

    Clean up of qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from qrutils import (
+        convert_numeric,
+        convert_alphanumeric,
+        bin
+        )
+
 from qrreference import (
         get_mode_indicators,
-        get_num_of_bits_character_count_indicator,
-        alphanumeric_codes
+        get_num_of_bits_character_count_indicator
         )
 
 correction_levels = ['L', 'M', 'Q', 'H']
@@ -52,7 +57,7 @@ class Encoder(object):
             assert self.validate_alphanumeric_bitstream_length()
 
     def _insert_indicators(self):
-        indicators = self.mode_bits + _bin(len(self.input_string), self.count_bits)
+        indicators = self.mode_bits + bin(len(self.input_string), self.count_bits)
         return indicators
 
     def _terminator(self):
@@ -103,104 +108,9 @@ class Encoder(object):
         """
         pass
 
-def handle_numeric_input(input):
-    data_bits = convert_numeric(input)
-    mode_bits = mode_indicator_bits()
-    count_bits = count_indicator_bits(input)
-    output = mode_bits + count_bits + data_bits
-    if validate_numeric_bitstream_length(input, output, len(count_bits)):
-        return output
-    else:
-        # waiting to build a QRException
-        raise Exception
-
-
-def _split_numeric_input(input):
-    splitted_data = []
-    tmp_string = ''
-    for i in range(1, len(input) + 1):
-        if (i % 3) == 0:
-            splitted_data.append(tmp_string + input[i-1])
-            tmp_string = ''
-        else:
-            tmp_string += input[i-1]
-    if tmp_string:
-        splitted_data.append(tmp_string)
-    return splitted_data
-
-def _split_alphanumeric_input(input):
-    splitted_data = []
-    tmp_list = []
-    for i in range(1, len(input) + 1):
-        if (i % 2) == 0:
-            tmp_list.append(input[i-1])
-            splitted_data.append(tmp_list)
-            tmp_list = []
-        else:
-            tmp_list.append(input[i-1])
-    if tmp_list:
-        splitted_data.append(tmp_list)
-    return splitted_data
-
-def convert_numeric(input):
-    splitted_input = _split_numeric_input(input)
-    data_bit_stream = ''
-    for input in splitted_input:
-        len_input = len(input)
-        data_bit_stream += _bin(int(input), (len_input * 3) + 1)
-    return data_bit_stream
-
-def convert_alphanumeric(input):
-    input = alphanumeric_codes(input)
-    splitted_input = _split_alphanumeric_input(input)
-    data_bit_stream = ''
-    for input in splitted_input:
-        if len(input) == 2:
-            data_bit_stream += _bin(input[0]*45+input[1], 11)
-        else:
-            data_bit_stream += _bin(input[0], 6)
-    return data_bit_stream
-
-def count_indicator_bits(input, version=1, data_type='numeric'):
-    if (data_type == 'numeric') and (version == 1):
-        return _bin(len(input), 10)
-
-def mode_indicator_bits(version=1):
-    return '0001'
-
-def _bin(x, width):
-    return ''.join(str((x>>i)&1) for i in xrange(width-1,-1,-1))
-
-def validate_numeric_bitstream_length(input, output, count_bits):
-    #to be implemented
-    r = len(input) % 3
-    if r != 0:
-        r = r * 3 + 1
-    return len(output) == (4 + count_bits + 10 * (len(input)/3) + r)
-
 
 ### TESTS
 
-def test_split_numeric_mode():
-    assert(_split_numeric_input('01234567') == ['012', '345', '67'])
-    assert(_split_numeric_input('') == [])
-
-def test_split_alphanumeric_mode():
-    assert(_split_alphanumeric_input([12,34,54,2,23]) == [[12, 34], [54,2],
-        [23]])
-    assert(_split_numeric_input('') == [])
-
-def test_convert_to_binary_repr():
-    assert(convert_numeric('01234567') == '000000110001010110011000011')
-
-def test_count_indicator_bits():
-    assert count_indicator_bits('01234567') == '0000001000'
-
-def test_complete():
-    assert(handle_numeric_input('01234567') ==
-            '00010000001000000000110001010110011000011')
-    assert(handle_numeric_input('0123456789012345') ==
-            '00010000010000000000110001010110011010100110111000010100111010100101')
 
 def test_numeric_encoder():
     e = Encoder('01234567', 'L')
@@ -214,18 +124,4 @@ def test_alphanumeric_encoder():
     e = Encoder('AC-42', 'L', 'alphanumeric')
     e.encode()
     assert e.code == '001000000010100111001110111001110010000100000'
-    
-
-
-__numeric__ = """
-The input data string is divided into groups of three digits, and each group
-is converted to its 10 bit binary equivalent. If the number of input digits is
-not an exact multiple of three, the final one or two digits are converted to 4
-or 7 bits respectively. The binary data is then concatenated and prefixed with
-the Mode Indicator and the Character Count Indicator. The Character Count
-Indicator in the Numeric Mode has 10, 12 or 14 bits as defined in Table 3. The
-number of input data characters is converted to its 10, 12 or 14 bit binary
-equivalent and added after the Mode Indicator and before the binary data
-sequence.
-"""
 
---
+++ qrutils.py
@@ -0,0 +1,64 @@
+from qrreference import alphanumeric_codes
+
+def split_numeric_input(input):
+    splitted_data = []
+    tmp_string = ''
+    for i in range(1, len(input) + 1):
+        if (i % 3) == 0:
+            splitted_data.append(tmp_string + input[i-1])
+            tmp_string = ''
+        else:
+            tmp_string += input[i-1]
+    if tmp_string:
+        splitted_data.append(tmp_string)
+    return splitted_data
+
+def split_alphanumeric_input(input):
+    splitted_data = []
+    tmp_list = []
+    for i in range(1, len(input) + 1):
+        if (i % 2) == 0:
+            tmp_list.append(input[i-1])
+            splitted_data.append(tmp_list)
+            tmp_list = []
+        else:
+            tmp_list.append(input[i-1])
+    if tmp_list:
+        splitted_data.append(tmp_list)
+    return splitted_data
+
+def bin(x, width):
+    return ''.join(str((x>>i)&1) for i in xrange(width-1,-1,-1))
+
+def convert_numeric(input):
+    splitted_input = split_numeric_input(input)
+    data_bit_stream = ''
+    for input in splitted_input:
+        len_input = len(input)
+        data_bit_stream += bin(int(input), (len_input * 3) + 1)
+    return data_bit_stream
+
+def convert_alphanumeric(input):
+    input = alphanumeric_codes(input)
+    splitted_input = split_alphanumeric_input(input)
+    data_bit_stream = ''
+    for input in splitted_input:
+        if len(input) == 2:
+            data_bit_stream += bin(input[0]*45+input[1], 11)
+        else:
+            data_bit_stream += bin(input[0], 6)
+    return data_bit_stream
+### TESTS
+
+def test_split_numeric_mode():
+    assert(split_numeric_input('01234567') == ['012', '345', '67'])
+    assert(split_numeric_input('') == [])
+
+def test_split_alphanumeric_mode():
+    assert(split_alphanumeric_input([12,34,54,2,23]) == [[12, 34], [54,2],
+        [23]])
+    assert(split_numeric_input('') == [])
+
+def test_convert_numeric():
+    assert(convert_numeric('01234567') == '000000110001010110011000011')
+
