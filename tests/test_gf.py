c3cf5177e253a6650b5ecbd8e098483146d571f7
Edoardo Batini <eodbat@gmail.com>
2011-04-01 01:41:35 +0200

    Use generator polynomials table in tests

--- tests/test_gf.py
+++ tests/test_gf.py
@@ -4,13 +4,14 @@
 import qrcode as qr
 from qrutils import list_to_coeff
 from array import array
+from rs_generator_polynomials import generator_polynomials
 import gf
 
 def test_reed_solomon():
     gf256 = gf.GaloisField()
     a = qr.Encoder('abcde123', 1, 'H', 'alphanumeric')
     A = gf.GFPoly(gf256, list_to_coeff(a.codewords)).multiply_by_monomial(17, 1)
-    b = [gf256.alpha_power(x) for x in [0, 43, 139, 206, 78, 43, 239, 123, 206, 214, 147, 24, 99, 150, 39, 243, 163, 136]]
+    b = [gf256.alpha_power(x) for x in generator_polynomials[17]]
     B = gf.GFPoly(gf256, b)
     q, r = A.divide(B)
     assert r.coefficients == array('i', [42, 159, 74, 221, 244, 169, 239, 150, 138, 70, 237, 85, 224, 96, 74, 219, 61])
@@ -22,7 +23,7 @@ def test_reed_solomon():
             '11101100', '00010001', '11101100', '00010001']
 
     A = gf.GFPoly(gf256, list_to_coeff(a.codewords)).multiply_by_monomial(10,1)
-    b = [gf256.alpha_power(x) for x in [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45]]
+    b = [gf256.alpha_power(x) for x in generator_polynomials[10]]
     B = gf.GFPoly(gf256, b)
     q,r = A.divide(B)
     assert r.coefficients[0] == 165
